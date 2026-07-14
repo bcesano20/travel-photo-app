7ed14db8d08a26e8dc58f932d4b49a74320522b2
bcesano20 <54218077+bcesano20@users.noreply.github.com>
2026-07-14 09:51:36 +0200

    add serializers

---
+++ backend/gallery/serializers/__init__.py
@@ -0,0 +1,10 @@
+from .album import AlbumDetailSerializer, AlbumListSerializer
+from .media import MediaSerializer
+from .share_link import ShareLinkSerializer
+
+__all__ = [
+    "AlbumListSerializer",
+    "AlbumDetailSerializer",
+    "MediaSerializer",
+    "ShareLinkSerializer",
+]
---
+++ backend/gallery/serializers/share_link.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+
+from gallery.models import ShareLink
+
+
+class ShareLinkSerializer(serializers.ModelSerializer):
+    """
+    password is write_only: it comes in as plaintext when creating the link,
+    but is stored hashed (see set_password() on the model). It is never
+    returned in responses.
+    """
+
+    password = serializers.CharField(
+        write_only=True, required=False, allow_blank=True
+    )
+    url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ShareLink
+        fields = [
+            "id",
+            "album",
+            "token",
+            "password",
+            "expires_at",
+            "is_active",
+            "created_at",
+            "url",
+        ]
+        read_only_fields = ["token", "created_at"]
+
+    def get_url(self, obj):
+        return f"/a/{obj.token}"
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", "")
+        instance = ShareLink(**validated_data)
+        instance.set_password(password)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        instance = super().update(instance, validated_data)
+        if password is not None:
+            instance.set_password(password)
+            instance.save()
+        return instance
