20cc5de27792bcbee579d7ca74731e7180a8c0a1
bcesano20 <54218077+bcesano20@users.noreply.github.com>
2026-07-14 09:51:25 +0200

    add admins

---
+++ backend/gallery/admin/__init__.py
@@ -0,0 +1,6 @@
+from .album import AlbumAdmin
+from .media import MediaAdmin, MediaInline
+from .share_link import ShareLinkAdmin
+from .user import CustomUserAdmin
+
+__all__ = ["CustomUserAdmin", "AlbumAdmin", "MediaAdmin", "MediaInline", "ShareLinkAdmin"]
---
+++ backend/gallery/admin/media.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from ..models import Media
+
+# This make the Media editable in the Album admin panel
+class MediaInline(admin.TabularInline):
+    model = Media
+    extra = 0
+    fields = ["type", "original_filename", "size", "processing_status", "order"]
+    readonly_fields = ["size", "processing_status"]
+
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ["original_filename", "album", "type", "processing_status", "size"]
+    list_filter = ["type", "processing_status"]
+    search_fields = ["original_filename", "storage_key"]
